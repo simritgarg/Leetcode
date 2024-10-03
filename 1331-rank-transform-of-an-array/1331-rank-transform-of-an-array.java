c136dfaa0aaebaedc0b2d254dee44e31a4425f16
Simrit Garg <simritgarg@gmail.com>
2024-10-03 15:02:32 +0530

    Update 1331-rank-transform-of-an-array.java

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -24,3 +24,33 @@ class Solution {
 
     }
 }
+
+Method-2
+    
+import java.util.*;
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        if (arr.length == 0) {
+            return new int[0]; // Handle edge case for empty array
+        }
+        int[] arr2 = Arrays.copyOf(arr,arr.length);
+        Arrays.sort(arr2);
+        Map<Integer,Integer> mp = new HashMap<>();
+        mp.put(arr2[0],1);
+        for(int i=1;i<arr.length;i++){
+            if(arr2[i]>arr2[i-1]){
+                mp.put(arr2[i],mp.get(arr2[i-1])+1);
+            }
+            else{
+                mp.put(arr2[i],mp.get(arr2[i-1]));
+            }
+            
+        }
+        int[] res = new int[arr.length];
+        for(int i=0;i<arr.length;i++){
+            res[i] = mp.get(arr[i]);
+        }
+        return res;
+
+    }
+}
