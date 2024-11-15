8ee322f627db7b19d2388e4d9b99035eedc83c25
Simrit Garg <simritgarg@gmail.com>
2024-11-16 00:19:45 +0530

    Time: 17 ms (11.26%), Space: 40.7 MB (7.21%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int fib(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return fibo(n,dp);
+        
+    }
+    public int fibo(int n, int[] dp){
+        if(n<=1) return n;
+        if(dp[n]!=-1) return dp[n];
+        return fibo(n-1,dp) + fibo(n-2,dp);
+    }
+}
