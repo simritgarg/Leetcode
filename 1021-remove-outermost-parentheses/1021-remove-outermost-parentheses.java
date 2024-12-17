c6929306fac23e0d75da3fcbc05c2ccf4eb25528
Simrit Garg <simritgarg@gmail.com>
2024-12-17 22:31:45 +0530

    Time: 17 ms (6.23%), Space: 45.5 MB (5.67%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -1,22 +1,45 @@
 class Solution {
     public String removeOuterParentheses(String s) {
-        Stack<Character> st = new Stack<>();
-        StringBuilder str = new StringBuilder("");
+
+        // Using Stack
+        // Stack<Character> st = new Stack<>();
+        // StringBuilder str = new StringBuilder("");
+        // for(int i=0;i<s.length();i++){
+        //     if(s.charAt(i)=='('){
+        //         if(st.size()>0){
+        //             str.append(s.charAt(i));
+        //         }
+        //         st.push(s.charAt(i));
+        //     }
+        //     else{
+        //         st.pop();
+        //         if(st.size()>0){
+        //             str.append(s.charAt(i));
+        //         }
+        //     }
+        // }
+        // return str.toString();
+
+        // Using String
+        String res = "";
+        int count = 0;
         for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='('){
-                if(st.size()>0){
-                    str.append(s.charAt(i));
-                }
-                st.push(s.charAt(i));
+            if(s.charAt(i) =='(' && count==0){
+                count++;
             }
-            else{
-                st.pop();
-                if(st.size()>0){
-                    str.append(s.charAt(i));
-                }
+            else if(s.charAt(i) =='(' && count>=1){
+                res += s.charAt(i);
+                count++;
+            }
+            else if( s.charAt(i) ==')' && count>1){
+                res += s.charAt(i);
+                count--;
+            }
+            else if(s.charAt(i) ==')' && count==1){
+                count--;
             }
         }
-        return str.toString();
+        return res;
 
         
     }
