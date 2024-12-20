22eb017f9f3a7573661346d0663705776028f449
Simrit Garg <simritgarg@gmail.com>
2024-12-20 11:39:22 +0530

    Time: 11 ms (87.79%), Space: 45.7 MB (41.19%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,35 +1,30 @@
 class Solution {
     public String frequencySort(String s) {
-        Map<Character,Integer> mp = new HashMap<>();
-        // why we can't sort hashmap -> it use hashset internally -> fetch data using hashcode
-        for(int i =0; i<s.length();i++){
-            char ch = s.charAt(i);
-            if(mp.containsKey(ch)){
-                mp.put(ch,mp.get(ch)+1);
-            }
-            else{
-                mp.put(ch,1);
-            }
+         Map<Character, Integer> mp = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            mp.put(c, mp.getOrDefault(c, 0) + 1);
         }
+
         int size = mp.size();
-        StringBuilder sb = new StringBuilder();
+        StringBuilder str = new StringBuilder();
         while(size-- > 0){
             int max = 0;
             char c = 'a';
-            for(Map.Entry<Character,Integer> entry : mp.entrySet()){
-                int val = entry.getValue();
-                if(val>max){
-                    max = val;
-                    c = entry.getKey();
+            for(Map.Entry<Character,Integer> e : mp.entrySet()){
+                int value = e.getValue();
+                if(max<value){
+                    max = value;
+                    c = e.getKey();
                 }
             }
             mp.remove(c);
             for(int i=1;i<=max;i++){
-                sb.append(c);
+                str.append(c);
             }
-        }
-        return sb.toString();
-        
 
+
+        }
+        return str.toString();
+        
     }
 }
