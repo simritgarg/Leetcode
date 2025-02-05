ac291a17559218f2af108111cd15520bf8588886
Simrit Garg <simritgarg@gmail.com>
2025-02-06 02:48:59 +0530

    Time: 2 ms (25.17%), Space: 44.2 MB (42.7%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,22 +1,41 @@
 class Solution {
     public int compress(char[] chars) {
-        int j = 0, count = 1;  
-        for (int i = 0; i < chars.length; i++) {
-            if (i + 1 < chars.length && chars[i] == chars[i + 1]) {
+        int idx = 0;
+        int j = 0;
+        int count = 0;
+        while(idx<chars.length){
+            char ch = chars[idx];
+            while(idx<chars.length && chars[idx] == ch){
                 count++;
-            } else {
-                chars[j++] = chars[i];
-                if (count > 1) {
-                    String countStr = Integer.toString(count);
-                    for (char c : countStr.toCharArray()) {
-                        chars[j++] = c; 
-                    }
-                }
-                count = 1;
+                idx++;
             }
+            chars[j++] = ch;
+            if(count>1){
+                String s = count + "";
+                for(char it: s.toCharArray()) chars[j++] = it;
+            }
+            count=0;
         }
-
         return j;
+
+        // using if-else
+        // int j = 0, count = 1;  
+        // for (int i = 0; i < chars.length; i++) {
+        //     if (i + 1 < chars.length && chars[i] == chars[i + 1]) {
+        //         count++;
+        //     } else {
+        //         chars[j++] = chars[i];
+        //         if (count > 1) {
+        //             String countStr = Integer.toString(count);
+        //             for (char c : countStr.toCharArray()) {
+        //                 chars[j++] = c; 
+        //             }
+        //         }
+        //         count = 1;
+        //     }
+        // }
+
+        // return j;
 
     }
 }
