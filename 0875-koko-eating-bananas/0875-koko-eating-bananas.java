bcd36a195cc54e90683db8663341ad2359c66e91
Simrit Garg <simritgarg@gmail.com>
2025-02-22 23:59:34 +0530

    Time: 25 ms (45.19%), Space: 45 MB (75.05%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,17 +1,13 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int min = Integer.MAX_VALUE;
         int max = 0;
         for(int i: piles){
-            min = Math.min(min, i);
             max = Math.max(max, i);
         }
-        for(int i = 1;i<=max;i++){
-            int timeTaken = calculate_Hrs(piles,i);
-            if(timeTaken <= h) return i;
-        }
-        return max;
+        return BS(piles,h,max);
     }
+        
+    
     public int calculate_Hrs(int[] piles, int k){
         int hrs = 0;
         for(int i=0;i<piles.length;i++){
@@ -19,14 +15,17 @@ class Solution {
         }
         return hrs;
     }
-    // public int BS(int min, int max){
-    //     int low = 0;
-    //     int high = max-min+1;
-    //     while(low < high){
-    //         int mid = low + (high-low)/2;
-
-    //     }
-    // }
+    public int BS(int[] piles, int h, int max){
+        int low = 1;
+        int high = max;
+        while(low < high){
+            int mid = low + (high-low)/2;
+            int t = calculate_Hrs(piles,mid);
+            if(t <= h) high = mid;
+            else low = mid+1;
+        }
+        return low;
+    }
 }
 // 3,6,7,11 - > min =3 and max =11
 // 3..11 k=min h--; if(time>h) k++ - > k=4
