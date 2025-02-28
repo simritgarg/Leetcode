8c64d0cfa22687f9bf997fdd99aa81d181053f3a
Simrit Garg <simritgarg@gmail.com>
2025-02-28 21:03:50 +0530

    Time: 0 ms (100%), Space: 50.4 MB (51.74%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -10,20 +10,15 @@ class Solution {
         // return 0;
 
         // for log(n) - Binary Search
-        // int left =0;
-        // int right = nums.length-1;
-        // while(left<right){
-        //     int mid = left + (right-left)/2;
-        //     if(mid%2==1) mid--;
-        //     if(nums[mid]!= nums[mid+1]) right =mid;
-        //     else left = mid +2;
-        // }
-        // return nums[left];
-        int res = 0;
-        for(int i: nums){
-            res ^= i;
+        int left =0;
+        int right = nums.length-1;
+        while(left<right){
+            int mid = left + (right-left)/2;
+            if(mid%2==1) mid--;
+            if(nums[mid]!= nums[mid+1]) right =mid;
+            else left = mid +2;
         }
-        return res;
+        return nums[left];
         
 
 
