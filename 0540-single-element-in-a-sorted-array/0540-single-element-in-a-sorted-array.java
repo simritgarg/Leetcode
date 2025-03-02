ca458f82919ba9d216a00f14b2f6aa51f620b27f
Simrit Garg <simritgarg@gmail.com>
2025-03-02 18:46:32 +0530

    Time: 0 ms (100%), Space: 50.3 MB (51.76%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -10,17 +10,30 @@ class Solution {
         // return 0;
 
         // for log(n) - Binary Search
-        int left =0;
-        int right = nums.length-1;
-        while(left<right){
-            int mid = left + (right-left)/2;
-            if(mid%2==1) mid--;
-            if(nums[mid]!= nums[mid+1]) right =mid;
-            else left = mid +2;
-        }
-        return nums[left];
-        
+        // int left =0;
+        // int right = nums.length-1;
+        // while(left<right){
+        //     int mid = left + (right-left)/2;
+        //     if(mid%2==1) mid--;
+        //     if(nums[mid]!= nums[mid+1]) right =mid;
+        //     else left = mid +2;
+        // }
+        // return nums[left];
 
+        // M-2 Binary Search
+        int n = nums.length;
+        if(n==1) return nums[0];
+        if(nums[0] != nums[1]) return nums[0];
+        if(nums[n-1] != nums[n-2]) return nums[n-1];
+        int left =1;
+        int right = n-2;
+        while(left<=right){
+            int mid = left + (right-left)/2;
+            if((nums[mid] != nums[mid-1]) && (nums[mid] != nums[mid+1])) return nums[mid];
+            if((mid%2==1 && nums[mid] == nums[mid-1])  || (mid%2==0 && nums[mid] == nums[mid+1])) left = mid+1;
+            else right = mid-1;
+        }
+        return -1;
 
     }
 }
