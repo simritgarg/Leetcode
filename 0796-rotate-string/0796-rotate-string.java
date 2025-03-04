dc0bfcafaecc9aabf840eeea9c89ce1b9541277d
Simrit Garg <simritgarg@gmail.com>
2025-03-05 02:30:33 +0530

    Time: 0 ms (100%), Space: 41.5 MB (49.33%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
+        
         if (s.length() != goal.length()) return false;
 
         // Method - 1
@@ -7,30 +8,19 @@ class Solution {
         // return str.contains(goal);
 
         //method - 2 (substring concept)
-        String str = s;
+        // String str = s;
+        // for (int i = 0; i < s.length(); i++) {
+        //     str = str.substring(1) + str.charAt(0);
+        //     if (str.equals(goal)) return true;
+        // }
+        // return false;
+
+        // Method - 3 (strings)
+        StringBuilder str = new StringBuilder(s);
         for (int i = 0; i < s.length(); i++) {
-            str = str.substring(1) + str.charAt(0);
-            if (str.equals(goal)) return true;
+            str.append(str.charAt(0)).deleteCharAt(0);
+            if (str.toString().equals(goal)) return true;
         }
         return false;
-
-
-        // StringBuilder res = new StringBuilder();
-        // StringBuilder str = new StringBuilder();
-        // for(int i=0;i<s.length();i++){
-        //     if(s.charAt(i)!=goal.charAt(i)){
-        //         str.append(s.charAt(i));
-        //     }
-        //     else{
-        //         res.append(s.charAt(i));
-        //     }
-            
-        // }
-        // res = res.append(str);
-        // return 
-
-
-        
-        
     }
 }
