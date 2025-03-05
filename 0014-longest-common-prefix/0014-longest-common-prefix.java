3fba548c55fed0c1fc507ec4487b035443cafcbd
Simrit Garg <simritgarg@gmail.com>
2025-03-05 21:15:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,57 +1,21 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) return "";
+        if (strs.length == 1) return strs[0];
 
-        //Method - 1
-        // if (strs == null || strs.length == 0) {
-        //     return "";
-        // }
-        // String longest = strs[0];
-        // for (String str : strs) {
-        //     if (str.length() > longest.length()) {
-        //         longest = str;
-        //     }
-        // }
-        // int n = longest.length();
-        // int[] match = new int[n];
-        // for (int i = 0; i < n; i++) {
-        //     char ch = longest.charAt(i);
-        //     boolean isCommon = true;
-
-        //     for (String str : strs) {
-        //         if (i >= str.length() || str.charAt(i) != ch) {
-        //             isCommon = false;
-        //             break;
-        //         }
-        //     }
-        //     if (isCommon) {
-        //         match[i] = 1;
-        //     } else {
-        //         break; 
-        //     }
-        // }
-        // StringBuilder result = new StringBuilder();
-        // for (int i = 0; i < n; i++) {
-        //     if (match[i] == 1) {
-        //         result.append(longest.charAt(i));
-        //     } else {
-        //         break;
-        //     }
-        // }
-
-        // return result.toString();
-
-
-        //Method - 2
-        StringBuilder ans = new StringBuilder();
         Arrays.sort(strs);
-        String first = strs[0];
-        String last = strs[strs.length-1];
-        for (int i=0; i<Math.min(first.length(), last.length()); i++) {
-            if (first.charAt(i) != last.charAt(i)) {
-                return ans.toString();
+        String s1 = strs[0];
+        String s2 = strs[strs.length-1];
+        // String s = "";
+        StringBuilder s = new StringBuilder();
+        for(int i=0; i<Math.min(s1.length(), s2.length()) ;i++){
+            if(s1.charAt(i) == s2.charAt(i)) {
+                // s += s1.charAt(i);
+                s.append(s1.charAt(i));
             }
-            ans.append(first.charAt(i));
+            else break;
         }
-        return ans.toString();
+        return s;
+        
     }
 }
