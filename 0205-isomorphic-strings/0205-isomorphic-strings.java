2f583739c194cb950cdcc458bf07bba5dca13a7a
Simrit Garg <simritgarg@gmail.com>
2025-03-05 19:18:50 +0530

    Time: 8 ms (80.72%), Space: 42.7 MB (24.87%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,17 +1,13 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-
-        if(s.length()!=t.length()) return false;
-
-        //Method - 1 (Using HashTable)
+        if(s.length() != t.length()) return false;
         int[] a = new int[256];
         int[] b = new int[256];
         for(int i=0;i<s.length();i++){
-            if(a[s.charAt(i)] != b[t.charAt(i)] ) return false;
+            if(a[s.charAt(i)] != b[t.charAt(i)]) return false;
             a[s.charAt(i)] = i+1;
             b[t.charAt(i)] = i+1;
         }
         return true;
-  
     }
 }
