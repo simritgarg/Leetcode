1c7862cc0fcd468d3f3b494f7789515e5cf82c7c
Simrit Garg <simritgarg@gmail.com>
2025-03-05 20:02:06 +0530

    Time: 2 ms (99.05%), Space: 42.3 MB (52.66%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -21,25 +21,40 @@ class Solution {
         // return str.toString();
 
         // Using String
-        String res = "";
+        // String res = "";
+        // int count = 0;
+        // for(int i=0;i<s.length();i++){
+        //     if(s.charAt(i) =='(' && count==0){
+        //         count++;
+        //     }
+        //     else if(s.charAt(i) =='(' && count>=1){
+        //         res += s.charAt(i);
+        //         count++;
+        //     }
+        //     else if( s.charAt(i) ==')' && count>1){
+        //         res += s.charAt(i);
+        //         count--;
+        //     }
+        //     else if(s.charAt(i) ==')' && count==1){
+        //         count--;
+        //     }
+        // }
+        // return res;
+
+        // using String solution - 2
         int count = 0;
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i) =='(' && count==0){
+        StringBuilder res = new StringBuilder();
+        for(char c: s.toCharArray()){
+            if(c == '('){
+                if(count != 0) res.append(c);
                 count++;
             }
-            else if(s.charAt(i) =='(' && count>=1){
-                res += s.charAt(i);
-                count++;
-            }
-            else if( s.charAt(i) ==')' && count>1){
-                res += s.charAt(i);
-                count--;
-            }
-            else if(s.charAt(i) ==')' && count==1){
+            else{
+                if(count!=1) res.append(c);
                 count--;
             }
         }
-        return res;
+        return res.toString();
 
         
     }
