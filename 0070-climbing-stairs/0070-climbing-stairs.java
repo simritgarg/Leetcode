2192b7ca69b2c240811aa438bc075beb79ee2efe
Simrit Garg <simritgarg@gmail.com>
2025-04-22 03:16:07 +0530

    Time: 0 ms (100%), Space: 41 MB (5.21%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,44 +1,14 @@
 class Solution {
-    
-    //MEMOIZATION
-    // public int climbStairs(int n) {
-    //     int[] dp = new int[n+1];
-    //     Arrays.fill(dp,-1);
-    //     return stairs(n,dp);
-        
-    // }
-    // public int stairs(int n, int[] dp){
-    //     if(n==0) return 1;
-    //     if(n==1) return 1;
-    //     if(dp[n]!=-1) return dp[n];
-    //     dp[n] = stairs(n-1,dp)+stairs(n-2,dp);
-    //     return dp[n];
-
-    // }
-
-    //TABULATION
-    // public int climbStairs(int n) {
-    //     if(n==0 || n==1) return 1;
-    //     int[] dp = new int[n+1];
-    //     dp[0] = 1;
-    //     dp[1] = 1;
-    //     for(int i=2;i<=n;i++){
-    //         dp[i] = dp[i-1] + dp[i-2];
-    //     }
-    //     return dp[n];
-        
-    // }
-
     public int climbStairs(int n) {
         int[] dp = new int[n+1];
         Arrays.fill(dp,-1);
         return ways(n,dp);
-
     }
     public int ways(int n, int[] dp){
         if(n==0) return 1;
         if(n<0) return 0;
-        if(dp[n]!=-1) return dp[n];
+
+        if(dp[n] != -1) return dp[n];
         dp[n] = ways(n-1,dp) + ways(n-2,dp);
         return dp[n];
     }
