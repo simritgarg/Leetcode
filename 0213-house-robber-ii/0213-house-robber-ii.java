d671d3b5a612988a5e38e7b7ce1902f7f6f929ec
Simrit Garg <simritgarg@gmail.com>
2025-04-22 02:55:57 +0530

    Time: 0 ms (100%), Space: 40.8 MB (91.03%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,30 +1,43 @@
 class Solution {
-    // public int rob(int[] nums) {
-    //     int[] dp = new int[nums.length];
-    //     Arrays.fill(dp,-1);
-    //     return robbery(nums,nums.length,dp);
+    //MEMOIZATION
+    public int rob(int[] nums) {
+        if (nums.length == 1) return nums[0];
+        int[] dp1 = new int[nums.length];
+        Arrays.fill(dp1, -1);
+        int max1 = robbery(nums, 0, nums.length - 2, dp1);
+        int[] dp2 = new int[nums.length];
+        Arrays.fill(dp2, -1);
+        int max2 = robbery(nums, 1, nums.length - 1, dp2);
+        return Math.max(max1, max2);
         
-    // }
-    // public int robbery(int[] nums, int index, int[] dp){
+    }
+    public int robbery(int[] nums, int start, int end , int[] dp){
+         if(start>end) return 0;
+        if(dp[start]!=-1) return dp[start];
 
-    // }
+        int pick = robbery(nums,start+2,end,dp)+ nums[start];
+        int skip = robbery(nums,start+1,end,dp);
+
+        return dp[start]= Math.max(pick,skip);
+
+    }
 
 
     //TABULATiON
-    public int rob(int[] nums) {
-        if(nums.length == 1) return nums[0];
-        return Math.max(robbery(nums,0,nums.length-2), robbery(nums,1,nums.length-1));
-    }
+    // public int rob(int[] nums) {
+    //     if(nums.length == 1) return nums[0];
+    //     return Math.max(robbery(nums,0,nums.length-2), robbery(nums,1,nums.length-1));
+    // }
 
-    public int robbery(int[] nums, int start, int end){
-        int prevRob = 0;
-        int maxRob = 0;
-        for(int i=start;i<=end;i++){
-            int temp = Math.max(maxRob,prevRob + nums[i]);
-            prevRob = maxRob;
-            maxRob = temp;
+    // public int robbery(int[] nums, int start, int end){
+    //     int prevRob = 0;
+    //     int maxRob = 0;
+    //     for(int i=start;i<=end;i++){
+    //         int temp = Math.max(maxRob,prevRob + nums[i]);
+    //         prevRob = maxRob;
+    //         maxRob = temp;
 
-        }
-        return maxRob;
-    }
+    //     }
+    //     return maxRob;
+    // }
 }
