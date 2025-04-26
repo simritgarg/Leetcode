dd93b00b452f0410daacaed49b798d8374190b23
Simrit Garg <simritgarg@gmail.com>
2025-04-26 17:12:38 +0530

    Time: 1 ms (38%), Space: 43.1 MB (77.4%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,43 +1,17 @@
 class Solution {
-
-    //MEMOIZATION
-    // public int minCostClimbingStairs(int[] cost) {
-    //     int n = cost.length;
-    //     int[] dp = new int[n];
-    //     Arrays.fill(dp, -1);
-    //     return Math.min(minCost(cost, n - 1, dp), minCost(cost, n - 2, dp));
-        
-    // }
-    // public int minCost(int[] cost, int index, int[] dp) {
-    //     if (index < 0) return 0; 
-    //     if (dp[index] != -1) return dp[index]; 
-    //     dp[index] = cost[index] + Math.min(minCost(cost, index - 1, dp), minCost(cost, index - 2, dp));
-    //     return dp[index];
-
-    // }
-
-    //TABULATION
-    // public int minCostClimbingStairs(int[] cost) {
-    // int n = cost.length;
-    // int[] dp = new int[n + 1];
-    // dp[0] = 0;
-    // dp[1] = 0; 
-    
-    // for (int i = 2; i <= n; i++) {
-    //     dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
-    // }
-    // return dp[n]; 
     public int minCostClimbingStairs(int[] cost) {
-        int n = cost.length;
-        int[] dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        return Math.min(minCost(n-1,cost,dp),minCost(n-2,cost,dp));
+        int[] dp1 = new int[cost.length];
+        Arrays.fill(dp1,-1);
+        int[] dp2 = new int[cost.length];
+        Arrays.fill(dp2,-1);
+        return Math.min(totalCost(cost,0,dp1),totalCost(cost,1,dp2));
     }
-    public int minCost(int n, int[] cost, int[] dp){
-        if(n<0) return 0;
-        if(dp[n]!=-1) return dp[n];
-        dp[n] = cost[n] + Math.min(minCost(n-1,cost,dp),minCost(n-2,cost,dp));
-        return dp[n];
+    public int totalCost(int[] cost, int idx, int[] dp){
+        // base case
+        if(idx >= cost.length) return 0;
+        if(dp[idx]!=-1) return dp[idx];
+        int pick = cost[idx] + totalCost(cost,idx+2,dp);
+        int skip = cost[idx] + totalCost(cost,idx+1,dp);
+        return dp[idx] = Math.min(pick,skip);
     }
-
 }
