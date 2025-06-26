d3b45045d73b9107bedfa211ed23685b48334459
Simrit Garg <simritgarg@gmail.com>
2025-06-26 17:34:39 +0530

    Time: 1 ms (82.27%), Space: 43.4 MB (46.99%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -6,10 +6,11 @@ class Solution {
     }
     // start is openBrackets and end is the closing brackets
     public void backtrack(String s, int start, int end, int n, List<String> res){
-        if(start == n && end == n){
-            res.add(s);
-            return;
-        }
+        // if(start == n && end == n){
+        //     res.add(s);
+        //     return;
+        // }
+        if(start == end && start + end == 2*n) res.add(s);
         if(start < n) backtrack(s+'(',start+1,end,n,res);
         if(end < start) backtrack(s+')',start,end+1,n,res);
     }
