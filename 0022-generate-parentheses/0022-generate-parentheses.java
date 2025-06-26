6dc46cc1999ce77d7af75dd1ba0b5671dc0dd454
Simrit Garg <simritgarg@gmail.com>
2025-06-26 17:23:44 +0530

    Time: 1 ms (82.27%), Space: 43.2 MB (58.99%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        backtrack("", 0, 0, n, res);
+        return res;
+    }
+    // start is openBrackets and end is the closing brackets
+    public void backtrack(String s, int start, int end, int n, List<String> res){
+        if(start == n && end == n){
+            res.add(s);
+            return;
+        }
+        if(start < n) backtrack(s+'(',start+1,end,n,res);
+        if(end < start) backtrack(s+')',start,end+1,n,res);
+    }
+}
