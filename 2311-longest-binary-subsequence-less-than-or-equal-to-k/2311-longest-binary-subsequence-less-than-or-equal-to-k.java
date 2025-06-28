d0389ee17a1587afb5cc81479862d51c0db14908
Simrit Garg <simritgarg@gmail.com>
2025-06-28 16:48:58 +0530

    Time: 1 ms (99.96%), Space: 41.8 MB (59.99%) - LeetHub

---
+++ 2311-longest-binary-subsequence-less-than-or-equal-to-k/2311-longest-binary-subsequence-less-than-or-equal-to-k.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int longestSubsequence(String s, int k) {
+        if(s.length() == 0){
+            return 0;
+        }
+        int zerocount = 0;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == '0'){
+                zerocount++;
+            }
+        }
+        int maxLength = 0;
+        long power = 1;
+        long value = 0;
+
+        for(int i=s.length()-1; i>=0; i--){
+            char ch = s.charAt(i);
+
+            if(ch == '1'){
+                if(value + power <= k){
+                    value += power;
+                    maxLength++;
+                }
+            }
+            power *= 2;
+            if(power > (long)k){
+                break;
+            }
+        }
+        return zerocount + maxLength;
+    }
+}
