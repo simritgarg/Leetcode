a3389899e80747584049596dc04ebc303c71257a
Simrit Garg <simritgarg@gmail.com>
2025-07-03 12:26:13 +0530

    Time: 0 ms (100%), Space: 43.1 MB (69.39%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,17 +1,27 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> res = new ArrayList<>();
-        backtrack("", 0, 0, n, res);
+        StringBuilder s = new StringBuilder();
+        backtrack(s, 0, 0, n, res);
         return res;
     }
     // start is openBrackets and end is the closing brackets
-    public void backtrack(String s, int start, int end, int n, List<String> res){
+    public void backtrack(StringBuilder s, int start, int end, int n, List<String> res){
         // if(start == n && end == n){
         //     res.add(s);
         //     return;
         // }
-        if(start == end && start + end == 2*n) res.add(s);
-        if(start < n) backtrack(s+'(',start+1,end,n,res);
-        if(end < start) backtrack(s+')',start,end+1,n,res);
+        // OR
+        if(start == end && start + end == 2*n) res.add(s.toString());
+        if(start < n){
+            backtrack(s.append('('),start+1,end,n,res);
+            s.deleteCharAt(s.length()-1);
+        } 
+        if(end < start){
+            backtrack(s.append(')'),start,end+1,n,res);
+            s.deleteCharAt(s.length()-1);
+        } 
     }
 }
+
+// To increase the time complexity we will usethe string builder
